31748e720565b08ebee7c65f7955289ea2f17f55
mcucchi9 <m.cucchi@bopen.it>
2019-03-15 12:09:37 +0100

    added class SimulationRunner()

--- sim/simulation.py
+++ sim/simulation.py
@@ -1,4 +1,6 @@
 import numpy as np
+import netCDF4 as nc
+import os
 
 
 def runge_kutta_4(x: list, f: float, fx, hs: float):
@@ -74,6 +76,9 @@ class SystemState:
 
 
 class Simulator:
+    """
+    Integrate point and system information,
+    """
 
     def __init__(self,
                  system=lorenz_96,
@@ -83,7 +88,6 @@ class Simulator:
                  forcing=0
                  ):
         """
-
         :param system: first-order differential equations system
         :param int_method: integration method
         :param: system_state: instance of SystemState class
@@ -115,8 +119,10 @@ class Simulator:
 
     def integrate(self, integration_time=None):
         """
+        Evolve the state of system_state, which automatically update the state of point.
 
-        :return:
+        :param integration_time: total time of integration. If None, integrate to the next time step.
+        :return: None
         """
         if integration_time is None:
             integration_time = self.increment
@@ -129,3 +135,62 @@ class Simulator:
             self.system_state.time = self.system_state.time + self.increment
 
 
+class SimulationRunner:
+
+    def __init__(self,
+                 simulator: Simulator,
+                 integration_time: int,
+                 chunk_length: int,
+                 out_file: str):
+        """
+        :param simulator: Simulator() instance
+        :param integration_time: total time of integration
+        :param chunk_length: length of each chunk (computed before saving to file), in terms of integration steps
+        :param out_file: path to output file
+        :return
+        """
+        self.simulator = simulator
+        self.integration_time = integration_time
+        self.chunk_length = chunk_length
+        self.out_file = out_file
+        self.integration_steps = int(self.integration_time / simulator.increment)
+        self.chunks = int(self.integration_steps / self.chunk_length)
+
+    def __init_netcdf(self,
+                      dataset):
+
+        dim = len(self.simulator.system_state.coords)
+
+        dataset.createDimension('node', dim)
+        dataset.createDimension('time', None)
+
+        times = dataset.createVariable('time', np.float64, ('time',))
+        nodes = dataset.createVariable('node', np.int32, ('node',))
+
+        var = dataset.createVariable('var', np.float32, ('time', 'node'))
+        nodes[:] = np.arange(0, dim)
+
+        return var, times, dim
+
+    def run(self):
+
+        # remove out file if already exists
+        if os.path.exists(self.out_file):
+            os.remove(self.out_file)
+
+        with nc.Dataset(self.out_file, 'w') as dataset:
+
+            var, times, dim = self.__init_netcdf(dataset)
+
+            for chunk in np.arange(0, self.chunks):
+                # initialize chunk numpy array
+                t = []
+                data_array = np.empty((self.chunk_length, dim))
+                # simulate one chunk
+                for i in np.arange(0, self.chunk_length):
+                    data_array[i, :] = self.simulator.system_state.coords
+                    t.append(self.simulator.system_state.time)
+                    self.simulator.integrate()
+                # write
+                var[(self.chunk_length * chunk):(self.chunk_length * (chunk + 1)), :] = data_array
+                times[(self.chunk_length * chunk):(self.chunk_length * (chunk + 1))] = t
