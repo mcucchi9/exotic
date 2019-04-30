88d871285ea1cdb1ff7ab65eec795001524bb035
Marco Cucchi <mcucchi9@gmail.com>
2019-04-30 17:50:13 +0100

    added file upload

--- notifierbot.py
+++ notifierbot.py
@@ -5,7 +5,17 @@ from slackclient import SlackClient
 sc = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 sc.api_call(
-"chat.postMessage",
-channel="#general",
-text="Hello from Python!"
+    "chat.postMessage",
+    channel="#general",
+    text="Hello from Python!"
 )
+
+with open('/home/marco/Pictures/prova.png', 'rb') as f:
+    sc.api_call(
+        "files.upload",
+        channels='#general',
+        filename='prova.png',
+        title='prova',
+        initial_comment='commento',
+        file=f
+    )
