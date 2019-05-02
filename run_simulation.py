feae1599c04951127b1c6cc6e3bf04d954b4c029
Marco Cucchi <m.cucchi@bopen.it>
2019-05-02 15:32:03 +0200

    changed

--- run_simulation.py
+++ run_simulation.py
@@ -1,45 +1,35 @@
-import os
-from sim import simulation as sim
-import numpy as np
-# import xarray as xr
-from netCDF4 import Dataset
+import sys
 
-point = sim.SystemState([1, 2, 3, 4, 5, 6], 0)
-traj = sim.Simulator(system_state=point)
+import lab.simulation.simulation as sim
+import lab.simulation.forcings as forcings
+import lab.simulation.systems as systems
+import lab.simulation.integrators as integrators
 
-integration_time = 10
-integration_steps = int(integration_time/traj.increment)
-chunk_steps = 100
-chunks = int(integration_steps/chunk_steps)
+sys.path.append('../')
 
-file_path = '/home/cucchi/phd/prova.nc'
+DATA_PATH = '../../data/'
 
-if os.path.exists(file_path):
-    os.remove(file_path)
+point_const = sim.SystemState(coords=[8]*32)
+point_const.perturbate()
+constant_force = forcings.ConstantForcing()
+system = systems.Lorenz96()
+int_method = integrators.RungeKutta4()
 
-dataset = Dataset(file_path, 'w')
+simulator_const = sim.Simulator(
+    system_state=point_const,
+    forcing=constant_force,
+    int_method=int_method,
+    system=system
+)
 
-lon = dataset.createDimension('node', len(point.coords))
-time = dataset.createDimension('time', None)
+runner = sim.SimulationRunner(
+    simulator=simulator_const,
+)
 
-times = dataset.createVariable('time', np.float64, ('time',))
-nodes = dataset.createVariable('node', np.int32, ('node',))
+runner.run(
+    integration_time=100,
+    chunk_length=10000,
+    write_all_every=1000,
+    data_base_path=DATA_PATH
+)
 
-var = dataset.createVariable('var', np.float32, ('time', 'node'))
-
-dim = len(point.coords)
-
-for chunk in np.arange(0, chunks):
-    # initialize chunk numpy array
-    print(chunk)
-    t = []
-    data_array = np.empty((chunk_steps, dim))
-    for i in np.arange(0, chunk_steps):
-        data_array[i, :] = point.coords
-        t.append(point.time)
-        traj.integrate()
-    # data_array = xr.DataArray(data_nparray, dims=('time', 'node'), coords={'time': t, 'node': [0, 1, 2, 3, 4, 5]})
-    # data_set = data_array.to_dataset(name='var')
-    # data_array = xr.DataArray(data_nparray)
-    # print(data_set)
-    # data_set.to_netcdf('/home/cucchi/phd/prova.nc', mode='a', unlimited_dims='time')
