1a8c0388a2cec965dd467692bd82088f0814e09b
Marco Cucchi <m.cucchi@bopen.it>
2019-05-02 16:39:37 +0200

    get DATA_PATH from environment

--- run_simulation.py
+++ run_simulation.py
@@ -1,13 +1,18 @@
 import sys
+import os
 
 import lab.simulation.simulation as sim
 import lab.simulation.forcings as forcings
 import lab.simulation.systems as systems
 import lab.simulation.integrators as integrators
 
+from slackclient import SlackClient
+# instantiate Slack client
+sc = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
 sys.path.append('../')
 
-DATA_PATH = '../data/'
+DATA_PATH = os.environ.get('BASE_DATA_PATH')
 
 point_const = sim.SystemState(coords=[8]*32)
 point_const.perturbate()
@@ -26,10 +31,17 @@ runner = sim.SimulationRunner(
     simulator=simulator_const,
 )
 
-runner.run(
+outfiles = runner.run(
     integration_time=100,
     chunk_length=10000,
     write_all_every=1000,
     data_base_path=DATA_PATH
 )
+
+for outfile in outfiles:
+    sc.api_call(
+        "chat.postMessage",
+        channel="#general",
+        text="Simulation {} done!".format(outfile)
+    )
 
