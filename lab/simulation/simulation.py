2ed293c2cf33633873a4fbf8e5037679e571092f
Marco Cucchi <m.cucchi@bopen.it>
2019-05-03 09:34:44 +0200

    simplified equality comparison

--- lab/simulation/simulation.py
+++ lab/simulation/simulation.py
@@ -243,9 +243,8 @@ class SimulationRunner:
 
         # check if dir exists. if not, create it.
         for outfile in outfiles:
-            if os.path.exists(os.path.dirname(outfile)) == False:
+            if not os.path.exists(os.path.dirname(outfile)):
                 os.makedirs(os.path.dirname(outfile))
-
 
         # remove out file if already exists
         for file_path in outfiles:
