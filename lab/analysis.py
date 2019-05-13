865bb22cc3496cdb19df2dfd1c15c083d60dcbbd
Marco Cucchi <m.cucchi@bopen.it>
2019-05-13 12:27:33 +0200

    completed compute_green_function()

--- lab/analysis.py
+++ lab/analysis.py
@@ -1,8 +1,17 @@
 import numpy as np
+import math
 
 
 def compute_green_function(
         series: np.ndarray
 ) -> np.ndarray:
 
-    pass
+    nt = len(series)
+    nfft = 2 ** (math.ceil(math.log2(abs(nt))))
+    series_extd = np.zeros(2 * nfft)
+    series_extd[0:len(series)] = series
+    series_extd = np.roll(series_extd, nfft)
+
+    chi = np.fft.fft(series_extd)
+
+    return chi
