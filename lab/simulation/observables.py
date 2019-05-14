f1dd90673e8d315ef4605426bf8c71934cb91834
Marco Cucchi <m.cucchi@bopen.it>
2019-05-14 16:37:13 +0200

    added Bin() class

--- lab/simulation/observables.py
+++ lab/simulation/observables.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import typing as T
 
 
 class Energy:
@@ -16,3 +17,29 @@ class Energy:
     def short_name(self):
 
         return 'energy'
+
+
+class Bin:
+
+    def __init__(
+            self,
+            data: xr.DataArray,
+            threshold: T.Union[T.List[float, ], T.List[float, float]]
+    ):
+
+        if len(threshold) == 1:
+            obs = data.where(data > threshold[0], 0)
+            obs = obs.where(obs == 0, 1)
+        else:
+            obs = data.where(threshold[0] < data < threshold[1], 0)
+            obs = obs.where(obs == 0, 1)
+
+        return obs
+
+    @property
+    def short_name(self):
+
+        if len(threshold) == 1:
+            return 'exceed_{}'.format(round(threshold[0], 1))
+        else:
+            return 'bin_{}_{}'.format(round(threshold[0], 1), round(threshold[1], 1))
