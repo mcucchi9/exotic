42204aab7e1039dc5aeea53a97b79135c22de4fd
Marco Cucchi <m.cucchi@bopen.it>
2019-05-14 12:33:10 +0200

    compute_green_function() -> compute_scusceptibiliy + specific to the case of reponses to unit step forcing

--- lab/analysis.py
+++ lab/analysis.py
@@ -2,16 +2,24 @@ import numpy as np
 import math
 
 
-def compute_green_function(
+def compute_susceptibility(
         series: np.ndarray
 ) -> np.ndarray:
+    """
+    Compute susceptiility chi if series is response to unit step forcing
+    :param series: response to unit step forcing
+    :return:
+    """
+
+    green_function = np.diff(series)
 
     nt = len(series)
     nfft = 2 ** (math.ceil(math.log2(abs(nt))))
-    series_extd = np.zeros(2 * nfft)
-    series_extd[0:len(series)] = series
-    series_extd = np.roll(series_extd, nfft)
 
-    chi = np.fft.fft(series_extd)
+    green_function_extd = np.zeros(2 * nfft)
+    green_function_extd[0:len(green_function)] = green_function
+    green_function_extd = np.roll(green_function_extd, nfft)
+
+    chi = np.fft.fft(green_function_extd)
 
     return chi
