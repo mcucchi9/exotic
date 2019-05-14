f194aa69e632e8c789f3bc5a587a0fe816e83840
Marco Cucchi <m.cucchi@bopen.it>
2019-05-14 15:33:56 +0200

    added compute_response()

--- lab/analysis.py
+++ lab/analysis.py
@@ -23,3 +23,26 @@ def compute_susceptibility(
     chi = np.fft.fft(green_function_extd)
 
     return chi
+
+
+def compute_response(
+        chi: np.ndarray,
+        forcing
+) -> np.ndarray:
+    """
+    Compute response to **forcing** given extended susceptibility **chi**
+    :param chi: extended susceptibility
+    :param forcing: forcing function (from lab.forcings)
+    :return:
+    """
+    nfft = int(len(chi)/2)
+    f = [forcing(t*0.01) - 8 for t in np.arange(0, nfft)]
+
+    f_extd = np.zeros(2 * nfft)
+    f_extd[nfft:(nfft + len(f))] = f
+
+    F_extd = np.fft.fft(f_extd)
+
+    resp = np.fft.ifft(chi * (F_extd))
+
+    return np.real(resp)
