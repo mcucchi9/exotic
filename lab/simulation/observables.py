1b05ce6da16d16a4285f9bdb6e6d469931d5d414
Marco Cucchi <m.cucchi@bopen.it>
2019-05-15 10:19:21 +0200

    fixed Bin()

--- lab/simulation/observables.py
+++ lab/simulation/observables.py
@@ -23,15 +23,21 @@ class Bin:
 
     def __init__(
             self,
-            data: xr.DataArray,
-            threshold: T.Union[T.List[float, ], T.List[float, float]]
+            threshold: T.List[float]
     ):
 
-        if len(threshold) == 1:
-            obs = data.where(data > threshold[0], 0)
+        self.threshold = threshold
+
+    def __call__(
+            self,
+            data=xr.DataArray
+    ):
+
+        if len(self.threshold) == 1:
+            obs = data.where(data > self.threshold[0], 0)
             obs = obs.where(obs == 0, 1)
         else:
-            obs = data.where(threshold[0] < data < threshold[1], 0)
+            obs = data.where(self.threshold[0] < data < self.threshold[1], 0)
             obs = obs.where(obs == 0, 1)
 
         return obs
@@ -39,7 +45,7 @@ class Bin:
     @property
     def short_name(self):
 
-        if len(threshold) == 1:
-            return 'exceed_{}'.format(round(threshold[0], 1))
+        if len(self.threshold) == 1:
+            return 'exceed_{}'.format(round(self.threshold[0], 1))
         else:
-            return 'bin_{}_{}'.format(round(threshold[0], 1), round(threshold[1], 1))
+            return 'bin_{}_{}'.format(round(self.threshold[0], 1), round(self.threshold[1], 1))
