fe7b3f5c3e571aa9dc1150a6f47f4ff4d9778573
Marco Cucchi <mcucchi9@gmail.com>
2020-01-03 14:22:46 +0000

    adds new files

---
+++ exotic/batch/concat_netcdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+#SBATCH --ntasks=1 #(default, so here optional)
+#SBATCH --cpus-per-task=1  #(default, so here optional)
+#SBATCH --job-name=concat_netcdf
+#SBATCH --output=concat_netcdf.out
+#SBATCH --error=concat_netcdf_err.out 
+#SBATCH --partition=cluster #(optional, default is cluster)
+#SBATCH --time=24:00:00 #(optional, default is 24 hours)
+#SBATCH --mem=4096
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mcucchi9@gmail.com
+
+python3 ../scripts/concat_netcdf.py ${1} ${2} ${3}
---
+++ exotic/scripts/check_broken_netcdf.py
@@ -0,0 +1,5 @@
+import sys
+
+import xarray as xr
+
+
